47eedcf8596dcb882fe5c23824e19ef651d005f1
Tomtom-debug <127477491+Tomtom-debug@users.noreply.github.com>
2024-07-04 23:50:15 -0400

    ad footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="border-t border-neutral-900 pt-8 pb-4 mt-16 text-center font-ntr text-neutral-500">
+      Built and Designed by Samuel with React, Tailwind CSS, and Framer Motion.
+      <div className="mt-4">
+        All rights reserved. ©
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+
+
