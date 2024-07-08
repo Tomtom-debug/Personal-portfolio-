2b668d31ab833065986a5737f51a3f4cb8f1ee68
Tomtom-debug <127477491+Tomtom-debug@users.noreply.github.com>
2024-07-08 17:07:11 -0400

    add download resume feature to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,15 @@ import { motion } from "framer-motion"
 
 const About = () => {
   return (
-    <div className= "border-b border-neutral-900 pb-4">
-        <h1 className="my-20 text-center text-4xl font-ntr font-bold">
+    <div id ="about" className= "border-b border-neutral-900 pb-4">
+        <motion.h1 
+         whileInView={{opacity:1, x:0}}
+         initial={{opacity:0, x:100}}
+         transition={{duration:1.5}}
+        className="my-20 text-center text-4xl font-ntr font-bold">
             / about
             <span className="text-green-bright font-ntr" > me</span>
-        </h1>
+        </motion.h1>
         <div className="flex flex-wrap">
             <motion.div 
             whileInView={{opacity:1, x:0}}
