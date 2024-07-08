5d6146c246684f9b280a09518d70cab329177343
Tomtom-debug <127477491+Tomtom-debug@users.noreply.github.com>
2024-07-08 17:11:11 -0400

    add framer motion to hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,15 @@
 import { HERO_CONTENT } from "../constants";
 import bitEmoji from "../assets/SamuelBit.png";
+import { motion } from "framer-motion"
+
+const container = (delay) =>({
+  hidden: { x: -100, opacity: 0 },
+  visible:{
+    x: 0,
+    opacity: 1,
+    transition:{duration: 0.5, delay: delay},
+  },
+});
 
 const Hero = () => {
   return (
@@ -7,19 +17,52 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-10 tracking-tight lg:mt-16 lg:text-5xl text-6xl font-bold font-ntr">hi, Samuel here.</h1>
-            <span className="mb-4 text-4xl tracking-tight text-gray-600 font-ntr">
+            <motion.h1 
+              id="home"
+              variants={container(0)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false, amount: 0.5 }}
+              className="pb-10 tracking-tight lg:mt-16 lg:text-5xl text-6xl font-bold font-ntr">
+              hi, Samuel here.
+            </motion.h1>
+            <motion.span 
+              variants={container(0.5)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false, amount: 0.5 }}
+              className="mb-4 text-2xl tracking-tight text-gray-600 font-ntr">
               learning and creating new stuff brings me joy.
-            </span>
-            <span className="bg-gradient-to-r bg-green-bright bg-clip-text text-4xl tracking-tight text-transparent font-ntr">
+            </motion.span>
+            <motion.span 
+              variants={container(1)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false, amount: 0.5 }}
+              className="bg-gradient-to-r bg-green-bright bg-clip-text text-3xl tracking-tight text-transparent font-ntr">
               Full Stack Developer
-            </span>
-            <p className="my-2 max-w-xl text-2xl py-6 font-light tracking-tighter font-ntr">{HERO_CONTENT}</p>
+            </motion.span>
+            <motion.p 
+              variants={container(1.5)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false, amount: 0.5 }}
+              className="my-2 max-w-xl text-2xl py-6 font-light tracking-tighter font-ntr">
+              {HERO_CONTENT}
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="w-full lg:w-3/4 flex justify-center lg:ml-16 lg:-mt-10"> 
-            <img src={bitEmoji} alt="Bit Emoji" className="w-3/4 max-w-full h-auto" /> 
+            <motion.img 
+              initial={{x: 100, opacity: 0}}
+              whileInView={{x: 0, opacity: 1}}
+              transition={{duration: 1, delay: 1.2}}
+              viewport={{ once: false, amount: 0.5 }}
+              src={bitEmoji} 
+              alt="Bit Emoji" 
+              className="w-3/4 max-w-full h-auto" 
+            /> 
           </div>
         </div>
       </div>
