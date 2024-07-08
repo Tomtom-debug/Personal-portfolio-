c0c9c746a89a4c94775d99248f9c1c6d7cf9333a
Tomtom-debug <127477491+Tomtom-debug@users.noreply.github.com>
2024-07-08 17:12:20 -0400

    add link tags to github and linkedin and email

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,30 @@ import { IoIosMail } from 'react-icons/io';
 
 const Navbar = () => {
   return (
-    <nav className=" text-lightest-slate font-ntr font-bold py-6 mb-20 flex items-center justify-between">
-      <div className="ml-16 flex items-center space-x-4">
-        <div className="text-xl">Samuel Tawiah</div>
-        <div className="hidden md:flex space-x-4">
-          <a href="/" className="hover:text-green-bright">Home</a>
-          <a href="/about" className="hover:text-green-bright">About</a>
-          <a href="/services" className="hover:text-green-bright">Experience</a>
-          <a href="/contact" className="hover:text-green-bright">Projects</a>
+    <div className="sticky top-0 z-10"> 
+      <nav className="text-lightest-slate font-ntr font-bold py-6 mb-20 flex items-center justify-between">
+        <div className="ml-16 flex items-center space-x-4">
+          <div className="text-xl">Samuel Tawiah</div>
+          <div className="hidden md:flex space-x-4">
+            <a href="#home" className="hover:text-green-bright">Home</a>
+            <a href="#about" className="hover:text-green-bright">About</a>
+            <a href="#experience" className="hover:text-green-bright">Experience</a>
+            <a href="#projects" className="hover:text-green-bright">Projects</a>
+          </div>
         </div>
-      </div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <IoIosMail className="hover:text-green-bright"/>
-        <FaLinkedin className="hover:text-green-bright"/>
-        <FaGithub className="hover:text-green-bright"/>
-      </div>
-    </nav>
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+          <a href="mailto:samueltawiah2003@gmail.com" className="hover:text-green-bright">
+            <IoIosMail />
+          </a>
+          <a href="https://www.linkedin.com/in/samuel-tawiah/" target="_blank" rel="noopener noreferrer" className="hover:text-green-bright">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/Tomtom-debug" target="_blank" rel="noopener noreferrer" className="hover:text-green-bright">
+            <FaGithub />
+          </a>
+        </div>
+      </nav>
+    </div>
   );
 }
 
